805cea20475e76794b06a8e26ddb9de9eb2dd65f
Παπαδόπουλος Αλέξανδρος <alpa09898@gmail.com>
2021-03-28 18:14:07 +0300

    .

---
+++ IMDB/Main.vb
@@ -0,0 +1,6 @@
+﻿Public Class Main
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim box = New LogIn
+        box.Show()
+    End Sub
+End Class
--- IMDB/My Project/Application.Designer.vb
+++ IMDB/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.IMDB.LogIn
+            Me.MainForm = Global.IMDB.Main
 
         End Sub
     End Class
