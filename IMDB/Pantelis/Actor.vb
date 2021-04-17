1e163defdb5d7ddd04c5b6f99d08efafdcdb9b78
Panteleimon Kampouris <35311350+Ntelos@users.noreply.github.com>
2021-04-17 04:00:53 +0300

    Actor and User class that will contain queries for those tables

---
+++ IMDB/Pantelis/Actor.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Data
+
+Public Class Actor
+    Private id As Integer
+    Private name As String
+    Private surname As String
+
+    Sub GetAllActors(ByRef results As DataTable)
+        Dim con As New Connection
+        con.RunQuery("select * from Actors", results)
+    End Sub
+End Class
---
+++ IMDB/Pantelis/User.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Data
+
+Public Class User
+    Private id As Integer
+    Private first_name As String
+    Private last_name As String
+    Private email As String
+    Private username As String
+    Private password As String
+
+    Sub GetAllUsers(ByRef results As DataTable)
+        Dim con As New Connection
+        con.RunQuery("select * from Users", results)
+    End Sub
+End Class
