c7b1495b39b61d80eec0e6388331809b4e150276
Panteleimon Kampouris <35311350+Ntelos@users.noreply.github.com>
2021-04-17 04:02:28 +0300

    A Class with DataTable print methods

---
+++ IMDB/Pantelis/Message.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Data
+
+Public Class Message
+    Sub ShowTable(ByVal table As DataTable)
+        For Each col As DataColumn In table.Columns
+            Console.Write("{0,-14}", col.ColumnName)
+        Next
+
+        Console.WriteLine()
+
+        For Each row As DataRow In table.Rows
+
+            For Each col As DataColumn In table.Columns
+
+                If col.DataType.Equals(GetType(DateTime)) Then
+                    Console.Write("{0,-14:d}", row(col))
+                ElseIf col.DataType.Equals(GetType(Decimal)) Then
+                    Console.Write("{0,-14:C}", row(col))
+                Else
+                    Console.Write("{0,-14}", row(col))
+                End If
+            Next
+
+            Console.WriteLine()
+        Next
+
+        Console.WriteLine()
+    End Sub
+
+    Sub ShowColTypes(ByVal table As DataTable)
+        For Each col As DataColumn In table.Columns
+            Console.Write("{0,-14}", col.DataType)
+        Next
+    End Sub
+End Class
