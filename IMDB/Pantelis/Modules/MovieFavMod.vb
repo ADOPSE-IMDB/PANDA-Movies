aa857c9f1138fdae59fa92938a3979c6d47e5da3
Nikos Mav <grnikos78@gmail.com>
2021-04-22 23:24:51 +0300

    load favorite table changed and added methods.

---
+++ IMDB/Pantelis/Modules/MovieFavMod.vb
@@ -0,0 +1,3 @@
+﻿Module MovieFavMod
+
+End Module
--- IMDB/Profile/Favorite.vb
+++ IMDB/Profile/Favorite.vb
@@ -18,58 +18,17 @@ Public Class Favorite
 
 
     Private Sub load_table()
-        Dim con As New Connection
-        Dim table As New DataTable()
-        Dim adapter As New MySqlDataAdapter()
-        Dim command As New MySqlCommand("   select distinct user_id,movie_id,title,year,description,rating from MovieLists inner join Movies on MovieLists.movie_id=Movies.id and MovieLists.user_id = @idd", con.getConnection())
 
-        command.Parameters.Add("@idd", MySqlDbType.VarChar).Value = 1
-        con.Connect()
-
-        Try
-            Using reader As MySqlDataReader = command.ExecuteReader()
-                While reader.Read()
-                    MessageBox.Show(reader.GetString(2), "Happ", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                End While
-
-                con.Disconnect()
-            End Using
-        Catch
-            con.Disconnect()
-
-        End Try
 
 
     End Sub
     Private Sub ButtonLoadTable_Click(sender As Object, e As EventArgs) Handles ButtonLoadTable.Click
 
 
-        MySqlConn = New MySqlConnection
-        MySqlConn.ConnectionString = "Server=dblabs.it.teithe.gr;Port=3306;Database=it185223;Uid=it185223;Pwd=chilli123;"
-        Dim SDA As New MySqlDataAdapter
-        Dim dbDataSet As New DataTable
-        Dim bSource As New BindingSource
-        Try
-            MySqlConn.Open()
-            Dim Querry As String
-            Querry = "select user_id,movie_id,title,year,description,rating from MovieLists inner join Movies on MovieLists.movie_id=Movies.id
-"
-
-            COMMAND = New MySqlCommand(Querry, MySqlConn)
-                SDA.SelectCommand = COMMAND
-            SDA.Fill(dbDataSet)
-            bSource.DataSource = dbDataSet
-            DataGridView1.DataSource = bSource
-            SDA.Update(dbDataSet)
-            MySqlConn.Close()
-
-        Catch ex As Exception
-            MessageBox.Show(ex.Message)
-        Finally
-            MySqlConn.Dispose()
-        End Try
 
     End Sub
 
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
 
+    End Sub
 End Class
