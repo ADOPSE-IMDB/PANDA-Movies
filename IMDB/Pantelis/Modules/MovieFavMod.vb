b0ec476d89a83f58f6968e9a1aa73a472c9e7163
Nikos Mav <grnikos78@gmail.com>
2021-04-23 19:25:15 +0300

    query changed mysql MovieFavMod

--- IMDB/Pantelis/Modules/MovieFavMod.vb
+++ IMDB/Pantelis/Modules/MovieFavMod.vb
@@ -31,8 +31,10 @@
         Dim con As New Connection
         Dim results As New DataTable
 
-        con.RunQuery("select distinct user_id,movie_id,title,year,description,rating,image_url from MovieFavorites 
-                      inner join Movies on MovieFavorites.movie_id=Movies.id and MovieFavorites.user_id =@0", args, results)
+        con.RunQuery(" 
+SELECT distinct user_id,movie_id,title,year,description,rating from MovieFavorites AS M
+                      inner join Movies as Mo on Mo.id=M.movie_id
+                      inner join Users as U on M.user_id=@0", args, results)
 
         Dim FavoriteCountMovies As Integer   'number of Favorite Movies 
 
