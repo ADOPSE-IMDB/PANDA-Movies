2192b54097df023ff41f5a209d92a7254a6454fd
Nikos Mav <grnikos78@gmail.com>
2021-05-02 23:58:18 +0300

    added RatedMovie Methods , updateRatedMovie.

--- IMDB/Pantelis/Modules/MovieMod.vb
+++ IMDB/Pantelis/Modules/MovieMod.vb
@@ -107,15 +107,42 @@ Module MovieMod
         Return topMovies
     End Function
 
-    Public Function RateMovie(ByVal movieID, ByVal userID, ByVal rating)
-        Dim flag As Boolean = False
+    Public Sub RateMovie(ByVal movieID, ByVal userID, ByVal rating)
+
 
         Dim con As New Connection
         Dim args() As String = {movieID, userID, rating}
         Dim results As New DataTable
         con.RunQuery("Insert into UserRating(movie_id,user_id,rating) values (@0,@1,@2)", args, results)
-        flag = True
-        Return flag
+
+
+    End Sub
+
+    Public Function CheckIfRated(ByVal movieID, ByVal userID)
+        Dim con As New Connection
+        Dim args() As String = {movieID, userID}
+        Dim results As New DataTable
+        con.RunQuery("Select * from UserRating where movieID=@0 and userID=@1", args, results)
+        If (results.Rows.Count.Equals(0)) Then
+            Return -1
+        Else
+
+            Return results.Rows(1)("rating").ToString
+
+        End If
     End Function
+
+    Public Sub UpdateRatedMovie(ByVal movieID, ByVal userID, ByVal rating)
+
+
+        Dim con As New Connection
+        Dim args() As String = {movieID, userID, rating}
+        Dim results As New DataTable
+        con.RunQuery("Update UserRating SET rating =@2 where user_id=@1 and movie_id=@0 ", args, results)
+
+
+    End Sub
+
+
 
 End Module
